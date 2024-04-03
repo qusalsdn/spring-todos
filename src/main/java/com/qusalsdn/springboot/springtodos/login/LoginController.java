a781b4d77719c906eab45dae2ca6a135e5a01995
변민우 <yja05092@gmail.com>
2024-04-03 16:55:59 +0900

    모델로 JSP에 로그인 자격 증명 표시하기

--- src/main/java/com/qusalsdn/springboot/springtodos/login/LoginController.java
+++ src/main/java/com/qusalsdn/springboot/springtodos/login/LoginController.java
@@ -1,12 +1,22 @@
 package com.qusalsdn.springboot.springtodos.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-    @RequestMapping("login")
+    @RequestMapping(value = "login", method = RequestMethod.GET)
     public String login() {
         return "login";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String welcome(@RequestParam String name, @RequestParam String password, ModelMap model) {
+        model.put("name", name);
+        model.put("password", password);
+        return "welcome";
     }
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Welcome Page</title>
+    </head>
+    <body>
+        <div>
+            Welcome to the qusalsdn!
+        </div>
+        <div>
+            Your Name: ${name}
+        </div>
+        <div>
+            Your Password: ${password}
+        </div>
+    </body>
+</html>
