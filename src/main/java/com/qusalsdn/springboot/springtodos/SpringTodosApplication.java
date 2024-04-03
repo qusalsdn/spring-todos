ca34fcc275307956bf4cf8481ed7fdd278b7686f
변민우 <yja05092@gmail.com>
2024-04-03 14:21:41 +0900

    Initial commit

---
+++ src/main/java/com/qusalsdn/springboot/springtodos/SpringTodosApplication.java
@@ -0,0 +1,13 @@
+package com.qusalsdn.springboot.springtodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qusalsdn/springboot/springtodos/SpringTodosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qusalsdn.springboot.springtodos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
