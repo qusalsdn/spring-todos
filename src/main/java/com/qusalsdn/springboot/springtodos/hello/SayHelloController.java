33f9e270263c4d493aa3cc6e47e378afa96c1fa4
변민우 <yja05092@gmail.com>
2024-04-03 15:11:09 +0900

    간단한 HTML 응답

--- src/main/java/com/qusalsdn/springboot/springtodos/hello/SayHelloController.java
+++ src/main/java/com/qusalsdn/springboot/springtodos/hello/SayHelloController.java
@@ -11,4 +11,22 @@ public class SayHelloController {
     public String sayHello() {
         return "Hello! What are you learning today?";
     }
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody // 메시지가 리턴한 것을 그대로 브라우저에 리턴하는 역할을 한다.
+    public String sayHelloHtml() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("<!DOCTYPE html>");
+        sb.append("<html lang=\"ko\">");
+        sb.append("<head>");
+        sb.append("<meta charset=\"UTF-8\">");
+        sb.append("<title>My first HTML Page</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("My First html page with body - Changed");
+        sb.append("</body>");
+        sb.append("</html>");
+
+        return sb.toString();
+    }
 }
