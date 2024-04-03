c0b7837815bd1ebed78c906f089743ca9f1f0032
변민우 <yja05092@gmail.com>
2024-04-03 23:46:12 +0900

    Todo를 추가하는 뷰 만들기

--- src/main/java/com/qusalsdn/springboot/springtodos/todo/TodoController.java
+++ src/main/java/com/qusalsdn/springboot/springtodos/todo/TodoController.java
@@ -3,6 +3,7 @@ package com.qusalsdn.springboot.springtodos.todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.List;
@@ -21,5 +22,15 @@ public class TodoController {
         List<Todo> todos = todoService.findByUserName("qusalsdn");
         model.put("todos", todos);
         return "listTodos";
+    }
+
+    @RequestMapping(value = "add-todo", method = RequestMethod.GET)
+    public String showNewTodoPage() {
+        return "todo";
+    }
+
+    @RequestMapping(value = "add-todo", method = RequestMethod.POST)
+    public String addNewTodo() {
+        return "redirect:list-todos";
     }
 }
