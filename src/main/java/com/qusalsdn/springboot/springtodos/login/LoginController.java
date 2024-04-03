8e09b345b0e5dd65fdb43feebbed828e61471c95
변민우 <yja05092@gmail.com>
2024-04-03 16:38:02 +0900

    Update LoginController.java

--- src/main/java/com/qusalsdn/springboot/springtodos/login/LoginController.java
+++ src/main/java/com/qusalsdn/springboot/springtodos/login/LoginController.java
@@ -1,7 +1,5 @@
 package com.qusalsdn.springboot.springtodos.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +7,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-    private Logger logger = LoggerFactory.getLogger(getClass());
-
     @RequestMapping("login")
     public String login(@RequestParam String name, ModelMap model) {
         model.put("name", name);
-        logger.debug("Request param is {}", name);
-        logger.info("I want this printed at info level");
-        logger.warn("I want this printed at info level");
         return "login";
     }
 }
