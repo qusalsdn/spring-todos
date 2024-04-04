4344bfd120528d7bb7c0ecf7efdefc6116db18e1
변민우 <yja05092@gmail.com>
2024-04-04 16:35:02 +0900

    Todo 엔터티를 만들고 Todo 데이터를 H2에 삽입
    spring.jpa.defer-datasource-initialization=true을 설정하면 테이블을 먼저 매핑한 후 sql 파일이 실행된다.

--- src/main/java/com/qusalsdn/springboot/springtodos/todo/Todo.java
+++ src/main/java/com/qusalsdn/springboot/springtodos/todo/Todo.java
@@ -1,10 +1,16 @@
 package com.qusalsdn.springboot.springtodos.todo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity
 public class Todo {
+    @Id
+    @GeneratedValue
     private int id;
     private String userName;
     @Size(min = 10, message = "Enter atleast 10 characters")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into todo (ID, USER_NAME, DESCRIPTION, TARGET_DATE, DONE) values(10001, 'qusalsdn', 'Get AWS Certified', CURRENT_DATE(), false)
+insert into todo (ID, USER_NAME, DESCRIPTION, TARGET_DATE, DONE) values(10002, 'qusalsdn', 'Get Azure Certified', CURRENT_DATE(), false)
+insert into todo (ID, USER_NAME, DESCRIPTION, TARGET_DATE, DONE) values(10003, 'qusalsdn', 'Get GCP Certified', CURRENT_DATE(), false)
+insert into todo (ID, USER_NAME, DESCRIPTION, TARGET_DATE, DONE) values(10004, 'qusalsdn', 'Learn DevOps', CURRENT_DATE(), false)
