219fc52106af610d822451fb236a7fdbd52a847d
변민우 <yja05092@gmail.com>
2024-04-04 16:14:49 +0900

    H2 콘솔을 사용하기 위한 Spring Security 설정

--- src/main/java/com/qusalsdn/springboot/springtodos/security/SpringSecurityConfiguration.java
+++ src/main/java/com/qusalsdn/springboot/springtodos/security/SpringSecurityConfiguration.java
@@ -2,11 +2,14 @@ package com.qusalsdn.springboot.springtodos.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -29,5 +32,14 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated()); // 모든 요청 승인
+        http.formLogin(Customizer.withDefaults()); // 승인되지 않은 요청이 있으면 formLogin을 설정해서 사용자 이름과 패스워드를 입력하는 페이지로 이동시킴
+        http.csrf(csrf -> csrf.disable()); // CSRF 비활성화
+        http.headers(header -> header.frameOptions(option -> option.disable())); // 프레임 비활성화
+        return http.build();
     }
 }
