798eb05bdd5448294f6e7543f9dc4ee06fab91bd
변민우 <yja05092@gmail.com>
2024-04-04 14:18:17 +0900

    Update WelcomeController.java

--- src/main/java/com/qusalsdn/springboot/springtodos/login/WelcomeController.java
+++ src/main/java/com/qusalsdn/springboot/springtodos/login/WelcomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @SessionAttributes("name") // 세션을 이용하면 다른 페이지로 넘어가도 값을 유지할 수 있다.
-public class LoginController {
+public class WelcomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String gotoWelcomePage(ModelMap model) {
         model.put("name", "qusalsdn");
