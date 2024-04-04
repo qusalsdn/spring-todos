66d579e408f7f254033fa938ac23708316328b83
변민우 <yja05092@gmail.com>
2024-04-04 13:37:35 +0900

    Todo 페이지에 날짜 추가

--- src/main/java/com/qusalsdn/springboot/springtodos/todo/TodoController.java
+++ src/main/java/com/qusalsdn/springboot/springtodos/todo/TodoController.java
@@ -40,7 +40,7 @@ public class TodoController {
         if (result.hasErrors()) {
             return "todo";
         }
-        todoService.addTodo((String) model.get("name"), todo.getDescription(), LocalDate.now().plusYears(1), false);
+        todoService.addTodo((String) model.get("name"), todo.getDescription(), todo.getTargetDate(), false);
         return "redirect:list-todos";
     }
 
